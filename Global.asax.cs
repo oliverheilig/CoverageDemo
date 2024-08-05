52bb052eb0b641b69f50d6fade15d3243fc49024
Oliver Heilig <oliver.heilig@ptvgroup.com>
2024-08-05 17:53:49 +0200

    Fix init

--- Global.asax.cs
+++ Global.asax.cs
@@ -13,13 +13,15 @@ namespace SpatialTutorial
 {
     public class Global : System.Web.HttpApplication
     {
-        public static string SpatiaLitePath;
-        public static string SpatiaLiteNativeDll = "libspatialite-4.dll";
         public static SQLiteConnection cn;
 
    
         protected void Application_Start(object sender, EventArgs e)
         {
+            // open the database
+            cn = new SQLiteConnection($@"Data Source={HostingEnvironment.MapPath("~/App_Data")}\db.sqlite;Version=3;");
+            cn.Open();
+
             // set PATH for SpatialLite depending on processor
             var mod_spatialite_folderPath = (IntPtr.Size == 4) ?
                  "mod_spatialite-5.1.0-win-x86" : "mod_spatialite-5.1.0-win-amd64";
@@ -28,9 +30,6 @@ namespace SpatialTutorial
                 HostingEnvironment.MapPath("~/SpatialLite/") + mod_spatialite_folderPath + ";" +
                 Environment.GetEnvironmentVariable("Path", EnvironmentVariableTarget.Process);
             Environment.SetEnvironmentVariable("Path", path, EnvironmentVariableTarget.Process);
-
-            cn = new SQLiteConnection($@"Data Source={HostingEnvironment.MapPath("~/App_Data")}\db.sqlite;Version=3;");
-            cn.Open();
 
             // load spatiallite extension
             cn.LoadExtension("mod_spatialite");
